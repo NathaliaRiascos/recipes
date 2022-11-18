42e23d29bb472dd7f7aaf5f47817eb4f5d9c2b35
nathaliariascos <riascosnathalia6@gmail.com>
2022-11-17 22:51:59 -0500

    fix(Root): error search recipe by name was resolved and redux persistor deleted

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -4,32 +4,12 @@ import { persistReducer, persistStore } from 'redux-persist'
 import thunk from 'redux-thunk'
 
 import { recipesSlice } from './features'
-const persistConfig = {
-  key: 'root',
-  version: 1,
-  storage,
-}
 
-const rootReducer = combineReducers({
-  recipes: recipesSlice.reducer,
+export const store = configureStore({
+  reducer: {
+    recipes: recipesSlice.reducer
+  }
 })
-
-export type RootReducer = ReturnType<typeof rootReducer>
-
-const persistedReducer = persistReducer<RootReducer, AnyAction>(
-  persistConfig,
-  rootReducer
-)
-
-const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }).concat(thunk)
-})
-
-export const persistor = persistStore(store)
 
 export type AppDispatch = typeof store.dispatch
 
